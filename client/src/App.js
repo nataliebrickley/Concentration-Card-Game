d3cf7da0aab229c06190a3932be09fec4cfa7e0a
Natalie Brickley <natt_rose@yahoo.com>
2020-01-25 19:03:39 -0800

    added a home screen and styling

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,15 @@ class App extends React.Component {
     data: [],
     first: "",
     second: "",
-    pairsLeft: 26
+    pairsLeft: 26,
+    playing: false
   }
 
   handleNewGame = () => {
     API.shuffle().then(res => {
       this.setState({ data: res.data })
     })
+    this.setState({ playing: true })
   }
   showCard = (item) => {
     item.show = true;
@@ -56,62 +58,78 @@ class App extends React.Component {
     }
   }
   render() {
-    let { data } = this.state
+    let { data, playing } = this.state
     return (
       <div className="App">
-        <div className="row">
-          {data.filter(item => data.indexOf(item) <= 12).map((item, idx) => {
-            return (
-              <Card
-                key={idx}
-                cardClick={() => this.cardClick(item)}
-                idx={idx}
-                item={item}
-              />
-            )
-          })}
-        </div>
+        {!playing
+          ? <div className="home">
+            <div className="homeText">
+              <p className="title">Concentration Card Game</p>
+              <p className="rules">Make as many pairs as you can!</p>
+              <div className="game-btn">
+                <button onClick={() => this.handleNewGame()}>New Game</button>
+              </div>
+            </div>
 
-        <div className="row">
-          {data.filter(item => data.indexOf(item) > 12 && data.indexOf(item) <= 25).map((item, idx) => {
-            return (
-              <Card
-                key={idx}
-                cardClick={() => this.cardClick(item)}
-                idx={idx}
-                item={item}
-              />
-            )
-          })}
-        </div>
+          </div>
+          : <div className="game">
+            <div className="row">
+              {data.filter(item => data.indexOf(item) <= 12).map((item, idx) => {
+                return (
+                  <Card
+                    key={idx}
+                    cardClick={() => this.cardClick(item)}
+                    idx={idx}
+                    item={item}
+                  />
+                )
+              })}
+            </div>
 
-        <div className="row">
-          {data.filter(item => data.indexOf(item) > 25 && data.indexOf(item) <= 38).map((item, idx) => {
-            return (
-              <Card
-                key={idx}
-                cardClick={() => this.cardClick(item)}
-                idx={idx}
-                item={item}
-              />
-            )
-          })}
-        </div>
+            <div className="row">
+              {data.filter(item => data.indexOf(item) > 12 && data.indexOf(item) <= 25).map((item, idx) => {
+                return (
+                  <Card
+                    key={idx}
+                    cardClick={() => this.cardClick(item)}
+                    idx={idx}
+                    item={item}
+                  />
+                )
+              })}
+            </div>
 
-        <div className="row">
-          {data.filter(item => data.indexOf(item) > 38 && data.indexOf(item) <= 51).map((item, idx) => {
-            return (
-              <Card
-                key={idx}
-                cardClick={() => this.cardClick(item)}
-                idx={idx}
-                item={item}
-              />
-            )
-          })}
-        </div>
+            <div className="row">
+              {data.filter(item => data.indexOf(item) > 25 && data.indexOf(item) <= 38).map((item, idx) => {
+                return (
+                  <Card
+                    key={idx}
+                    cardClick={() => this.cardClick(item)}
+                    idx={idx}
+                    item={item}
+                  />
+                )
+              })}
+            </div>
 
-        <button onClick={() => this.handleNewGame()}>New Game</button>
+            <div className="row">
+              {data.filter(item => data.indexOf(item) > 38 && data.indexOf(item) <= 51).map((item, idx) => {
+                return (
+                  <Card
+                    key={idx}
+                    cardClick={() => this.cardClick(item)}
+                    idx={idx}
+                    item={item}
+                  />
+                )
+              })}
+            </div>
+            <div className="game-btn">
+              <button onClick={() => this.handleNewGame()}>New Game</button>
+            </div>
+          </div>}
+
+
       </div>
     );
   }
